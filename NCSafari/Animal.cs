141164f1c598acdb01d050a1e25a3c84d0574602
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-18 11:26:11 +0000

    feat: initialised project

---
+++ NCSafari/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NCSafari
+{
+    public abstract class Animal
+    {
+        public float WeightInKg { get; set; }
+        public Animal(float weightInKg)
+        {
+           this.WeightInKg = weightInKg;
+        }
+
+        public abstract string MakeSound(string soundName)  ;
+
+    }
+}
---
+++ NCSafari/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NCSafari
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
